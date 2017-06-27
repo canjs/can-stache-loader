fb7fa97bcca6e5a2a2c1401b7ec81991434f87d6
Maciej Adamczak <madamc@softserveinc.com>
2017-06-27 11:39:11 +0200

    clean-up code

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const getIntermediateAndImports = require('./intermediate_and_imports');
-// const getIntermediateAndImports = require('can-stache/src/intermediate_and_imports');
-// const getIntermediateAndImports = require('can/dist/cjs/view/stache/intermediate_and_imports');
-
+const getIntermediateAndImports = require('can-stache/src/intermediate_and_imports');
 
 const getTemplate = (source, imports) => {
   const requires = imports.map(i => `require('${i}');`).join('\n');
 
-  return `var stache = require('can/dist/cjs/view/stache/stache');
-var mustacheCore = require('can/dist/cjs/view/stache/mustache_core');
-var getIntermediateAndImports = require('can/dist/cjs/view/stache/intermediate_and_imports');
+  return `var stache = require('can-stache');
+var mustacheCore = require('can-stache/src/mustache_core');
+var getIntermediateAndImports = require('can-stache/src/intermediate_and_imports');
 
 ${requires}
 
